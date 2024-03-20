acc38ca0731979d9d46c286d86ef2a5b1369a68d
Sean Parnell <seanpatrickparnell@gmail.com>
2024-03-19 23:57:25 -0700

    subCategory icons for Men,Women

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -6,18 +6,33 @@ const categorySchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  order: {
+    type: Number,
+    required: true
+  },
   parentCategory: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
   },
-  subCategories: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
+  subCategories: [{
+    name: {
+      type: String,
+      required: true,
+    },
+    order: {
+      type: Number,
+      required: true,
+    },
+    iconPath: {
+      type: String, // Assuming the icon path is a string
+      required: true,
+    },
+  }],
 }, {
   timestamps: true,
 });
 
+categorySchema.index({ name: 1 }, { unique: true });
 const Category = mongoose.model("Category", categorySchema);
 
 export default Category;
--- backend/models/parentCategoryModel.js
+++ backend/models/parentCategoryModel.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 
 const parentCategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
+  order: { type: Number, required: true } 
 });
 
 const ParentCategory = mongoose.model('ParentCategory', parentCategorySchema);
