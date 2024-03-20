50e6e05aaabf1371878372a6a39ae94e992fc03a
Sean Parnell <seanpatrickparnell@gmail.com>
2024-03-20 15:57:23 -0700

    Implement registerScreen

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,41 @@
 import React from "react";
-import { Navbar, Nav, Container, Image, Badge } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Image,
+  Badge,
+  NavDropdown,
+} from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {useLogoutMutation} from '../slices/usersApiSlice';
+import { logout } from "../slices/authSlice";
 import { LinkContainer } from "react-router-bootstrap";
 import LogoText from "../assets/main/logoText.png";
 import CategoryBar from "./CategoryBar";
 
 const NavBar = () => {
-  const cartItems = useSelector((state) => state.cart.cartItems);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
   const uniqueItemsCount = cartItems.length;
+  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login')
+    } catch (err) {
+      console.log(err)
+    }
+  };
 
   return (
     <header style={{ borderBottom: "1px solid grey" }}>
@@ -30,19 +56,28 @@ const NavBar = () => {
             // transition: "padding 0.3s ease",
           }}
         >
-        
-            <LinkContainer to="/">
-              <Navbar.Brand>
-                <img style={{ height: 25 }} src={LogoText} alt="logo" />
-              </Navbar.Brand>
-            </LinkContainer>
-          
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img style={{ height: 25 }} src={LogoText} alt="logo" />
+            </Navbar.Brand>
+          </LinkContainer>
 
-          <Container style={{ display: "flex" }}>
+          {userInfo ? (
+            <NavDropdown title={userInfo.name} id="username">
+              <LinkContainer to="/profile">
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Item onClick={logoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
             <Nav.Link href="/login">
               <FaUser />
             </Nav.Link>
-          
+          )}
+
+          <Container style={{ display: "flex" }}>
             <Nav.Link href="/cart">
               <div style={{ position: "relative" }}>
                 <RiShoppingCartLine
@@ -67,7 +102,6 @@ const NavBar = () => {
               </div>
             </Nav.Link>
           </Container>
-          
         </Container>
       </Navbar>
       <CategoryBar />
