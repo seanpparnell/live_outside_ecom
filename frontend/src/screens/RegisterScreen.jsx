37989a03aae6a745799992c6336139f9e7ccf087
Sean Parnell <seanpatrickparnell@gmail.com>
2024-03-20 17:12:04 -0700

    user registration

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCredentials } from '../slices/authSlice';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 import Loader from '../components/Loader';
 import {useRegisterMutation } from '../slices/usersApiSlice';
 import { toast } from 'react-toastify';
@@ -49,60 +49,60 @@ const RegisterScreen = () => {
   }
 
   return (
-    <FormContainer style={{display: 'flex', alignItems: 'center', justifyContent: 'center', alignContent: 'center'}}>
-      <h1>Register</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='name'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='enter name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId='email'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          >
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='confirm password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          >
-          </Form.Control>
-        </Form.Group>
-        <Button type='submit' variant='primary' disabled={isLoading}>
-          Register
-        </Button>
-        { isLoading && <Loader />}
-      </Form>
-      {/* <Row>
-        <Col>
-          New Customer ? <Link to={ redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
-        </Col>
-      </Row> */}
-    </FormContainer>
+      <FormContainer>
+        <h1>Register</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='confirm password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            >
+            </Form.Control>
+          </Form.Group>
+          <Button type='submit' variant='primary' disabled={isLoading}>
+            Register
+          </Button>
+          { isLoading && <Loader />}
+        </Form>
+        <Row>
+          <Col>
+            Already have an account ? <Link to={ redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
+          </Col>
+        </Row>
+      </FormContainer>
   )
 }
 
