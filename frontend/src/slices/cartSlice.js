de212ac374c9aee1ed12f86e0994f19bc1934598
Sean Parnell <seanpatrickparnell@gmail.com>
2024-03-29 14:05:59 -0700

    initial paymentScreen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -56,10 +56,14 @@ const cartSlice = createSlice({
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
       return updateCart(state)
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      return updateCart(state);
     }
   }
 });
 
-export const { addToCart, removeFromCart, updateCartItemQuantity, saveShippingAddress } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateCartItemQuantity, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
 
 export default cartSlice.reducer;
