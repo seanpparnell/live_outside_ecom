fd177b949ab7912eee0df304ad3def908a9864a7
Sean Parnell <seanpatrickparnell@gmail.com>
2024-05-08 15:32:48 -0700

    paypal integration / userApiSlice profileScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -25,6 +26,9 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -40,6 +44,8 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
       </Route>
     </Route>
   )
@@ -50,7 +56,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <PayPalScriptProvider deferLoading={true} >
+          <RouterProvider router={router} />
+        </PayPalScriptProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
